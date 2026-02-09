0a7dfebcc56894f6f788253eb439019c9facfdd8
Isadora Moraes <isadora.moraes@trinove.com.br>
2026-02-09 17:17:40 -0300

    Initial HealthConnect Angular project

---
+++ src/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'app-home',
+  imports: [MatToolbarModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}

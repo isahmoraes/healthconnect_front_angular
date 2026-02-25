c96c7cfb45ae4deee0901df5f3634710366fa5b3
Isadora Moraes <isadora.moraes@trinove.com.br>
2026-02-25 11:46:55 -0300

    ajust

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,15 +4,14 @@ import { Home } from '../home/home';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {  MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { Menu } from "../menu/menu";
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule, MatSidenavContainer, Menu],
+  imports: [RouterOutlet,MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
